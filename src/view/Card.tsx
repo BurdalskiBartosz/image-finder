760a80d73db2bd9abf2797adf1db6358a4a8f970
Bartosz <bartosz.burdalski@gmail.com>
2024-12-04 06:20:28 +0100

    Add navigations back on routes

--- src/view/Card.tsx
+++ src/view/Card.tsx
@@ -1,14 +1,21 @@
 import ImageCard from "../components/ImageCard/ImageCard";
 import { useApp } from "../hooks/useApp";
+import { useNavigate } from "react-router";
+import Button from "../components/Button/Button";
 
 function CardView() {
   const { previewData } = useApp();
+  const navigate = useNavigate();
+
   return (
-    <div className="container flex gap-8 py-8">
+    <div className="container flex flex-col gap-8 py-8">
       <div className="flex-grow h-fit grid grid-cols-3 gap-8">
         {!!previewData && (
           <ImageCard key={previewData.imgUrl} {...previewData} />
         )}
+      </div>
+      <div className="w-min">
+        <Button handleClick={() => navigate("/")}>Generate again</Button>
       </div>
     </div>
   );
--- src/view/ImagePreview.tsx
+++ src/view/ImagePreview.tsx
@@ -1,16 +1,23 @@
 import ImagePreview from "../components/ImagePreview/ImagePreview";
 import { useApp } from "../hooks/useApp";
+import { useNavigate } from "react-router";
+import Button from "../components/Button/Button";
 
 function ImagePreviewView() {
   const { previewData, refetch, save } = useApp();
+  const navigate = useNavigate();
+
   return (
-    <div className="container flex gap-8 py-8">
+    <div className="container flex flex-col gap-8 py-8">
       <div className="flex flex-col gap-y-4">
         <ImagePreview
           refetch={refetch}
           save={save}
           imgUrl={previewData?.imgUrl}
         />
+      </div>
+      <div className="w-min">
+        <Button handleClick={() => navigate(-1)}>Back</Button>
       </div>
     </div>
   );
